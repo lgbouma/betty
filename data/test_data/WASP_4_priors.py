12df2f4481844c8e8d3bad53e5708d1c8fe59672
lgbouma <bouma.luke@gmail.com>
2020-08-17 11:40:10 -0700

    add test_data.WASP_4_priors

---
+++ data/test_data/WASP_4_priors.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# normal: mu, sd
+# uniform: lower, upper, testval
+# ImpactParameter: testval
+
+priordict = {
+'r_star': ('Normal', 0.893, 0.034),
+'logg_star': ('Normal', 4.47, 0.11),
+'period': ('Normal', 1.338231466, 0.005),
+'t0': ('Normal', 1355.1845, 0.02),
+'log_r': ('Uniform', np.log(1e-2), np.log(1), np.log(0.15201)),
+'b': ('ImpactParameter', 0.5),
+'u[0]': ('Uniform', 0.382-0.2, 0.382+0.2, 0.382),
+'u[1]': ('Uniform', 0.210-0.2, 0.210+0.2, 0.210),
+'tess_mean': ('Normal', 1, 0.1)
+}
